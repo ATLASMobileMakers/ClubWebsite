f9bbeacde25c818d04b0b60002161169099baa34
Sigrunn Sky <sigrunn.sky@colorado.edu>
2016-03-30 16:52:14 -0600

    Converting to PHP

---
+++ mobilemakersio/includes/navbar.php
@@ -0,0 +1,34 @@
+
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+                <!--<img src="img/whitesitelogo.png" style="width: 50%"/>-->
+                <a href="index.html">ATLAS Mobile Makers</a>
+
+            </li>
+            <li>
+                <!--<a href="mobilemakersio/index2.html">Home</a>-->
+                <a href="index.html">Home</a>
+            </li>
+            <li>
+                iOS
+            </li>
+
+                <li>
+                    <a href="tableview.html">Table Views</a>
+                </li>
+
+            <li>
+                Android
+            </li>
+
+                <li>
+                    <a href="feelings.html">Intro</a>
+                </li>
+
+        </ul>
+    </div>
+</div>
