3699536538f6086266c584d3a8200f9935b794b9
Michael Swisher <Michael.V.Swisher@colorado.edu>
2016-09-12 14:43:27 -0400

    Fixed tutorials page.

---
+++ mobilemakersio/posts/includes/navbar.php
@@ -0,0 +1,33 @@
+
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+                <!--<img src="img/whitesitelogo.png" style="width: 50%"/>-->
+                <a href="../index.html">ATLAS Mobile Makers</a>
+
+            </li>
+            <li>
+                <a href="../index.html">Home</a>
+            </li>
+            <li>
+                iOS
+            </li>
+
+                <li>
+                    <a href="tableview.php">Table Views</a>
+                </li>
+
+            <li>
+                Android
+            </li>
+
+                <li>
+                    <a href="feelings.php">Intro</a>
+                </li>
+
+        </ul>
+    </div>
+</div>
